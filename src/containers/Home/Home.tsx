d7158e4bb2ef9bab5fc38f575b1c657ab6a31861
Nelli Gorlova <nelli7035@gmail.com>
2024-10-20 06:57:33 +0600

    Added the Home container

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-3">Home Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, nihil.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolorem exercitationem harum natus quasi quisquam quo rem vitae. A ab accusamus eveniet ipsa vel? Dolorem, totam!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad placeat quidem sunt vitae. Corporis, error?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolor dolores est fugiat inventore labore obcaecati, placeat quos. Aliquid cupiditate facere maxime odit omnis! Ad beatae facere hic inventore nam, non quaerat ut vitae voluptatibus!</p>
+    </div>
+  );
+};
+
+export default Home;
