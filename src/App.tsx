83057bdc0debfbd1adb276d1ba060939a167599f
Nelli Gorlova <nelli7035@gmail.com>
2024-10-21 04:19:46 +0600

    Added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,24 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home/Home.tsx';
 import AboutUs from './containers/AboutUs/AboutUs.tsx';
 import Contacts from './containers/Contacts/Contacts.tsx';
+import Footer from './components/Footer/Footer.tsx';
+
 
 const App = () => {
-
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/about" element={<AboutUs/>}/>
-        <Route path="/contact" element={<Contacts/>}/>
-      </Routes>
-    </>
-  )
+      <div style={{ flex: '1' }}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about" element={<AboutUs/>}/>
+          <Route path="/contact" element={<Contacts/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  );
 };
 
-export default App
+export default App;
